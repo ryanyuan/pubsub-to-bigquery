7a881cc0f45b1a2458753e186241b553e7c6b6a7
ryanyuan <ryan.yuan@outlook.com>
2019-07-01 16:54:07 +1000

    Refactor the code

--- src/main/java/org/ryanyuan/PubSubToBigQueryOptions.java
+++ src/main/java/org/ryanyuan/PubSubToBigQueryOptions.java
@@ -3,20 +3,27 @@ package org.ryanyuan;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.ValueProvider;
 
 public interface PubSubToBigQueryOptions extends DataflowPipelineOptions {
-    @Description("Pub/Sub subscription name.")
-    @Default.String("subscription")
-    String getPubSubSubscription();
-    void setPubSubSubscription(String pubSubSubscription);
+    @Description("Table spec to write the output to")
+    ValueProvider<String> getOutputTableSpec();
+    void setOutputTableSpec(ValueProvider<String> value);
 
-    @Description("BigQuery dataset name.")
-    @Default.String("dataset")
-    String getBigQueryDataset();
-    void setBigQueryDataset(String bigQueryDataset);
+    @Description("Pub/Sub topic to read the input from")
+    ValueProvider<String> getInputTopic();
+    void setInputTopic(ValueProvider<String> value);
 
-    @Description("BigQuery table name.")
-    @Default.String("table")
-    String getBigQueryTable();
-    void setBigQueryTable(String bigQueryTable);
+    @Description(
+            "The Cloud Pub/Sub subscription to consume from. "
+                    + "The name should be in the format of "
+                    + "projects/<project-id>/subscriptions/<subscription-name>.")
+    ValueProvider<String> getInputSubscription();
+    void setInputSubscription(ValueProvider<String> value);
+
+    @Description(
+            "This determines whether the template reads from " + "a pub/sub subscription or a topic")
+    @Default.Boolean(true)
+    Boolean getUseSubscription();
+    void setUseSubscription(Boolean value);
 }
--- src/main/java/org/ryanyuan/PubSubToBigQueryPipeline.java
+++ src/main/java/org/ryanyuan/PubSubToBigQueryPipeline.java
@@ -1,16 +1,13 @@
 package org.ryanyuan;
 
-import com.google.api.services.bigquery.model.TableRow;
-import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.InsertRetryPolicy;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
-import org.joda.time.Duration;
 import org.ryanyuan.transforms.JsonMsgToTableRow;
 
 
@@ -18,12 +15,10 @@ import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposi
 import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition.WRITE_APPEND;
 
 /**
- * Dataflow streaming pipeline to read messages from PubSub subscription and write the
+ * Dataflow streaming pipeline to read messages from PubSub and write the
  * payload to BigQuery
  */
 public class PubSubToBigQueryPipeline {
-    private static final String SUBSCRIPTION = "projects/%s/subscriptions/%s";
-    private static final String BIGQUERY_DATASET_TABLE = "%s:%s.%s";
 
     public static void main(String[] args) {
         PipelineOptionsFactory.register(PubSubToBigQueryOptions.class);
@@ -31,17 +26,43 @@ public class PubSubToBigQueryPipeline {
                 .withValidation()
                 .as(PubSubToBigQueryOptions.class);
 
+        run(options);
+
+    }
+    /**
+     * Runs the pipeline to completion with the specified options. This method does not wait until the
+     * pipeline is finished before returning. Invoke {@code result.waitUntilFinish()} on the result
+     * object to block until the pipeline is finished running if blocking programmatic execution is
+     * required.
+     *
+     * @param options The execution options.
+     * @return The pipeline result.
+     */
+    public static PipelineResult run(PubSubToBigQueryOptions options) {
         Pipeline pipeline = Pipeline.create(options);
 
-        PCollection<PubsubMessage> messages = pipeline
-                .apply("GetDataFromPubSub", PubsubIO.readMessagesWithAttributes().fromSubscription(String.format(SUBSCRIPTION, options.getProject(), options.getPubSubSubscription())))
-                .apply("ApplyWindow", Window.into(FixedWindows.of(Duration.standardSeconds(1))));
+        PCollection<PubsubMessage> messages;
 
-        PCollection<TableRow> rows = messages.apply("JsonToRow", new JsonMsgToTableRow());
+        if (options.getUseSubscription()) {
+            messages = pipeline
+                    .apply("Read from Pub/Sub subscription", PubsubIO.readMessagesWithAttributes().fromSubscription(options.getInputSubscription()));
+        } else {
+            messages = pipeline
+                    .apply("Read from Pub/Sub topic", PubsubIO.readMessagesWithAttributes().fromTopic(options.getInputTopic()));
+        }
 
-        rows.apply("WriteToBigQuery", BigQueryIO.writeTableRows().to(String.format(BIGQUERY_DATASET_TABLE, options.getProject(), options.getBigQueryDataset(), options.getBigQueryTable())).withoutValidation()
-                .withSchema(JsonMsgToTableRow.getSchema()).withCreateDisposition(CREATE_IF_NEEDED)
-                .withWriteDisposition(WRITE_APPEND));
-        pipeline.run();
+        messages.apply("Convert JSON to row", new JsonMsgToTableRow())
+                .apply("Write to BigQuery",
+                        BigQueryIO.writeTableRows()
+                                .withExtendedErrorInfo()
+                                .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
+                                .withFailedInsertRetryPolicy(InsertRetryPolicy.retryTransientErrors())
+                                .withSchema(JsonMsgToTableRow.getSchema())
+                                .to(options.getOutputTableSpec())
+                                .withoutValidation()
+                                .withCreateDisposition(CREATE_IF_NEEDED)
+                                .withWriteDisposition(WRITE_APPEND));
+
+        return pipeline.run();
     }
 }
